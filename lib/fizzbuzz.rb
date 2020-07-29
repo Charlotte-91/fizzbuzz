dfe0382508fa1e2c3248f61635b6a15dd755b127
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-07-29 09:15:15 +0000

    fizzbuzz finished

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-     return 'fizzbuzz'
+    return 'fizzbuzz'
   elsif (number % 3) == 0
     return  'fizz'
   elsif (number % 5) == 0
     return 'buzz'
-  elsif number == 2
-    return 2
+  else return number
   end
 end
